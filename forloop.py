3c1a16a761e72c903780b700c479dbf1aded76da
manishe417-oss <manishe417@gmail.com>
2025-09-01 15:22:18 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,7 @@
+a=int(input("enter your starting number number"))
+b=int(input("enter your ending number number"))
+c=0
+for i in range(a,b+1):
+   c=c+i
+print("the sum of number from",a,"to",b,"is",c)
+
---
+++ if.py
@@ -0,0 +1,7 @@
+a=input("enter your number")
+a=int(a)
+
+if a%2==0:
+    print("is a even number")
+else:
+    print(a,"is a odd number")
